e502f7cd534d01223710cac71255714d93df3679
hasinthaindrajee <hasinthaindrajee@gmail.com>
2018-01-26 14:43:28 +0530

    Binding identity.core initialized event to make sure configs are properly loaded

--- src/main/java/org/wso2/carbon/identity/oauth/client/auth/sample/internal/OAuth2SampleClientAuthnServiceComponent.java
+++ src/main/java/org/wso2/carbon/identity/oauth/client/auth/sample/internal/OAuth2SampleClientAuthnServiceComponent.java
@@ -23,6 +23,10 @@ import org.apache.commons.logging.LogFactory;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.wso2.carbon.identity.core.util.IdentityCoreInitializedEvent;
 import org.wso2.carbon.identity.oauth.client.auth.sample.OAuthSampleClientAuthenticator;
 import org.wso2.carbon.identity.oauth2.client.authentication.OAuthClientAuthenticator;
 
@@ -31,15 +35,34 @@ import org.wso2.carbon.identity.oauth2.client.authentication.OAuthClientAuthenti
         immediate = true
 )
 public class OAuth2SampleClientAuthnServiceComponent {
+
     private static Log log = LogFactory.getLog(OAuth2SampleClientAuthnServiceComponent.class);
 
     @Activate
     protected void activate(ComponentContext context) {
+
         try {
             OAuthSampleClientAuthenticator oAuthSampleClientAuthenticator = new OAuthSampleClientAuthenticator();
             context.getBundleContext().registerService(OAuthClientAuthenticator.class.getName(), oAuthSampleClientAuthenticator, null);
         } catch (Throwable e) {
             log.error("Error while activating OAuth2 sample client authenticator.", e);
         }
+    }
+
+    protected void unsetIdentityCoreInitializedEventService(IdentityCoreInitializedEvent identityCoreInitializedEvent) {
+        /* reference IdentityCoreInitializedEvent service to guarantee that this component will wait until identity core
+         is started */
+    }
+
+    @Reference(
+            name = "identity.core.init.event.service",
+            service = IdentityCoreInitializedEvent.class,
+            cardinality = ReferenceCardinality.MANDATORY,
+            policy = ReferencePolicy.DYNAMIC,
+            unbind = "unsetIdentityCoreInitializedEventService"
+    )
+    protected void setIdentityCoreInitializedEventService(IdentityCoreInitializedEvent identityCoreInitializedEvent) {
+        /* reference IdentityCoreInitializedEvent service to guarantee that this component will wait until identity core
+         is started */
     }
 }
